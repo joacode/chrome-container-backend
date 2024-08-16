40558844a1a95076d8bc6a0f33dd3150141cab13
Joaquin Perez Primavera <jperez.ext@ticmas.com>
2024-08-15 22:02:47 -0300

    first commit

--- src/entities/common/domain/wine-metadata.ts
+++
@@ -1,8 +0,0 @@
-import { BaseEntity } from './base-entity';
-
-export interface WineMetadata extends BaseEntity {
-  name?: string;
-  keyword: string;
-  enabled?: boolean;
-  image_url?: string;
-}
